9fa99d132283c00030c26a1727d7819c738afcd0
George M <giorgos_matlis@hotmail.com>
2021-09-28 00:41:36 +0300

    Create hackerRankClassTemplates.cpp

---
+++ HackerRank/hackerRankClassTemplates.cpp
@@ -0,0 +1,71 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+/*Write the class AddElements here*/
+
+template<class T>
+class AddElements {
+    public:
+        T element;
+        AddElements(T arg):element(arg){}
+        T add(T elem){
+            return this->element + elem;
+        }
+        string concatenate(string element) {
+            return this->element + element;
+        }
+        
+};
+
+template <>
+class AddElements <string> {
+    public:
+        string element;
+        AddElements(string elem):element(elem) {}
+        string concatenate(string element) {
+            return this->element + element;
+        }
+};
+
+int start_up() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
+
+int static r = start_up();
+
+#define endl '\n';
+
+int main () {
+  int n,i;
+  cin >> n;
+  for(i=0;i<n;i++) {
+    string type;
+    cin >> type;
+    if(type=="float") {
+        double element1,element2;
+        cin >> element1 >> element2;
+        AddElements<double> myfloat (element1);
+        cout << myfloat.add(element2) << endl;
+    }
+    else if(type == "int") {
+        int element1, element2;
+        cin >> element1 >> element2;
+        AddElements<int> myint (element1);
+        cout << myint.add(element2) << endl;
+    }
+    else if(type == "string") {
+        string element1, element2;
+        cin >> element1 >> element2;
+        AddElements<string> mystring (element1);
+        cout << mystring.concatenate(element2) << endl;
+    }
+  }
+  return 0;
+}
